043c65f9e62d9a88c78ef162521eff6e87eb1bd5
Saurav Gharti <sp.gharti@gmail.com>
2024-03-18 18:20:02 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fancy-violet-2",
+  url: "https://fancy-violet-2.botics.co",
+  api: "https://fancy-violet-2.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
